bcb5984453a04d88ce448656c5e80912fed2e152
Hakyeon Manuel Kim <khy@aooen.com>
2021-03-29 19:11:01 +0900

    Fix missing rules export at index.js (#7)

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const noClassnamesWithOneArgument = require('./rules/no-classnames-with-one-argument')
 const hooksDepsElementNewline = require('./rules/hooks-deps-element-newline')
+const noClassnamesWithOneArgument = require('./rules/no-classnames-with-one-argument')
+const pascalCaseEnumName = require('./rules/pascal-case-enum-name')
+const pascalCaseInterfaceName = require('./rules/pascal-case-interface-name')
+const pascalCaseTypeName = require('./rules/pascal-case-type-name')
 
 module.exports = {
   rules: {
-    'no-classnames-with-one-argument': noClassnamesWithOneArgument,
     'hooks-deps-element-newline': hooksDepsElementNewline,
+    'no-classnames-with-one-argument': noClassnamesWithOneArgument,
+    'pascal-case-enum-name': pascalCaseEnumName,
+    'pascal-case-interface-name': pascalCaseInterfaceName,
+    'pascal-case-type-name': pascalCaseTypeName,
   },
 }
