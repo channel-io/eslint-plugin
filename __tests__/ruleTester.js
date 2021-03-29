df9aed7505790a47c358a9f4344e4aa0789e8a08
Hakyeon Manuel Kim <khy@aooen.com>
2021-03-29 16:49:19 +0900

    Change parser to typescript-eslint/parser

--- __tests__/ruleTester.js
+++ __tests__/ruleTester.js
@@ -1,6 +1,8 @@
 const { RuleTester } = require('eslint')
+const typescriptParser = require.resolve('@typescript-eslint/parser')
 
 module.exports = new RuleTester({
+  parser: typescriptParser,
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
